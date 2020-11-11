f25d32718b7a9d72b1e45b85d98aacf6b1285931
Laura <laura.vivas.gamboa@gmail.com>
2020-11-11 15:41:45 +0100

    navbar updated and styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,31 @@
 import React, {useState} from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
 import About from "./About"
+import Projects from "./Projects"
 import Others from "./Others"
 import Contact from "./Contact"
-import './Navbar.css'
+import '../styles/navbar.css'
+
 
 
 function Navbar() {
   return (
     <div>
       <Router>
-        <div class="navbar navbar-expand-lg navbar-light bg-light">
-          <p class="navbar-brand">Laura Vivas //  Web Developer</p>
-          <div className='navbar-nav mr-auto'>
-            <NavLink ClassName="active" to='/'>Home</NavLink>
-            <NavLink ClassName="" to='/about'>About</NavLink>
-            <NavLink ClassName="" to='/others'>Others</NavLink>
-            <NavLink ClassName="" to='/contact'>Contact</NavLink>
+        <div class="navBar">
+          <p class="company">Laura Vivas</p>
+          <div className='links'>
+            <NavLink ClassName="link active" to='/'>Home</NavLink>
+            <NavLink ClassName="link" to='/about'>About</NavLink>
+            <NavLink ClassName="link" to='/project'>Projects</NavLink>
+            <NavLink ClassName="link" to='/others'>Others</NavLink>
+            <NavLink ClassName="link" to='/contact'>Contact</NavLink>
           </div>
         </div>
         <Switch>
           <Route exact path="/"></Route>
           <Route exact path="/about"><About/></Route>
+          <Route exact path="/project"><Projects/></Route>
           <Route exact path="/others"><Others/></Route>
           <Route exact path="/contact"><Contact/></Route>
         </Switch>
